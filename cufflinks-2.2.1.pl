f1184a148378e1c2e16f1bc3180659a2f88002b8
upendrak <upendra_35@yahoo.com>
2016-03-07 13:46:34 -0700

    change the script completely

--- cufflinks-2.2.1.pl
+++ cufflinks-2.2.1.pl
@@ -1,29 +1,34 @@
 #!/usr/bin/perl -w
 use strict;
-use warnings;
+use File::Copy qw/move copy/;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
+use Data::Dumper;
 
-#use constant CUFFLINKS  => 'cufflinks-2.0.0.Linux_x86_64/cufflinks';
-use constant CUFFLINKS  => '/cufflinks-2.2.1.Linux_x86_64/cufflinks';
+use constant CUFFLINKS  => ('2.2.1' => '/cufflinks-2.2.1.Linux_x86_64/');
+
+report_input_stack();
 
 # Define worflow options
-my ($query_file, $query_dir, $annotation, $user_annotation);
+my (@query_file, $query_dir, $annotation, $user_annotation, $version, $mask_file, $soft, $bias_fasta, $user_fasta);
+$soft = 1;
+$version = '2.2.1';
 
-
-GetOptions( "infile=s" => \$query_file,
-	    "indir=s"  => \$query_dir,
-	    "G=s"      => \$annotation,
-            "M=s"      => \$user_annotation,
+GetOptions( "infile=s"    => \@query_file,
+	    "G=s"         => \$annotation,
+            "M=s"         => \$user_annotation,
+	    "mask-file=s" => \$mask_file,
+	    "version=s"   => \$version,
+	    "fasta=s"     => \$bias_fasta,
+	    "user_fasta=s"=> \$user_fasta,
 	    );
 
+# more with this later, when they fix BOOLEANs
+my $hard = 1 unless $soft;
+
 my (@queries,$success);
-if ($query_dir) {
-    while (<$query_dir/*>) {
-	push @queries, $_;
-    }
-}
-if ($query_file) {
-    push @queries, $query_file;
+
+if (@query_file) {
+    push @queries, @query_file;
 }
 
 @queries > 0 || die "I could not find any SAM/BAM input files.\n";
@@ -33,6 +38,7 @@ if ($query_file) {
 # May not need to do this going forward...
 if (defined($user_annotation)) {
     $annotation = $user_annotation;
+    `dos2unix $user_annotation`;
 }
 
 # Grab any flags or options we don't recognize and pass them as plain text
@@ -41,31 +47,76 @@ my @args_to_reject = qw(-xxxx);
 my $CUFFLINKS_ARGS = join(" ", @ARGV);
 foreach my $a (@args_to_reject) {
     if ($CUFFLINKS_ARGS =~ /$a/) {
-	report("Mostarguments are legal for use with this script, but $a is not. Please omit it and submit again");
+	report("Most arguments are legal for use with this script, but $a is not. Please omit it and submit again");
 	exit 1;
     }
 }
 
-    my $app = CUFFLINKS;
-    my $cmd = "$app $CUFFLINKS_ARGS -G $annotation ";
+my %app = CUFFLINKS;
+my $clpath = $app{$version} or die "Cufflinks version $version is not supported."; 
+chomp($ENV{PATH} = `echo \$PATH`);
+$ENV{PATH} = join(':',$ENV{PATH},$clpath);
 
-for my $query_file (@queries) {    
-    chomp(my $basename = `basename $query_file`);
-    $basename =~ s/\.\S+$//;
-    my $cuffcommand = $cmd . $query_file;
-    report("$cuffcommand");
+my $cufflinks = $clpath . "cufflinks -o cufflinks_out";
+my $g = $hard ? '-G' : "-g"; # use GTF as a guideline (soft) or only consider annotated transcripts (hard)
+my $cmd = $annotation ? "$cufflinks $CUFFLINKS_ARGS $g $annotation " : "$cufflinks $CUFFLINKS_ARGS";
 
-    system("$cuffcommand");
 
-    mkdir $basename;
-    system("mv genes.fpkm_tracking isoforms.fpkm_tracking transcripts.gtf $basename");
-    
-    $success++ if -e "$basename/transcripts.gtf";
+if ($mask_file) {
+    `dos2unix $mask_file`;
+    $cmd .= "-M $mask_file ";
 }
 
-$success ? exit 0 : exit 1;
+if ($user_fasta) {
+    `dos2unix $user_fasta`;
+    $bias_fasta = $user_fasta;
+}
+
+if ($bias_fasta) {
+    $cmd .= " -b $bias_fasta";
+}
+
+my $gtf_out = 'gtf';
+for my $query_file (@queries) {    
+    my $basename = $query_file;
+    $basename =~ s/^\S+\/|\.\S+$//g;
+
+    my $cuffcommand = $cmd . " $query_file";
+    report("Executing: $cuffcommand");
+
+    system("$cuffcommand");
+    $success++ if -e "cufflinks_out/transcripts.gtf" && ! -z "cufflinks_out/transcripts.gtf";
+    system("mv cufflinks_out $basename\_out");
+
+    mkdir $gtf_out unless -d $gtf_out;
+    system("cp $basename\_out/transcripts.gtf $gtf_out/$basename\_transcripts.gtf");
+}
+
+system "rm -f *.fai";
+
+die "Something did not work, not transcripts.gtf file!" unless $success;
 
 sub report {
     print STDERR "$_[0]\n";
+}
+
+sub report_input_stack {
+    my @stack = @ARGV;
+    my %arg;
+
+    while (@stack) {
+        my $k = shift @stack;
+        my $v = shift @stack;
+        if ($v =~ /^-/) {
+            unshift @stack, $v;
+            $v = 'TRUE';
+        }
+        push @{$arg{$k}}, $v;
+    }
+
+    report("Input parameters:");
+    for (sort keys %arg) {
+        report(sprintf("%-25s",$_) . join(',',@{$arg{$_}}));
+    }
 }
 
